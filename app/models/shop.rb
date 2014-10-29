26aab835b26d39209dc1974a94ef90cc070f6ada
ruben2m <ruben2m@gmail.com>
2014-10-29 15:13:03 +0100

    Terminada versión beta

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,20 @@ class Shop < ActiveRecord::Base
   has_many :users, through: :manages
   has_many :menus
   has_many :days
+
+  def todaymenus
+    menus = []
+
+    # devolvemos los menus de hoy
+    dia = Time.zone.now.wday # 1 al 7
+    d = self.days
+    if(d.count > 0)
+      d = self.days.find_by_day_of_week(dia)
+      if(d != nil)
+        menus = d.menus
+      end
+    end
+
+    return menus
+  end
 end
